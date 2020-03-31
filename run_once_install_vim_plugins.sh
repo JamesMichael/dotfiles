35833758157b84d9dad4c731981ceff5f499a422
James Michael <jamesamichael@gmail.com>
2020-03-31 11:13:40 +0100

    Automatically install vim plugins

---
+++ run_once_install_vim_plugins.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install vim8 plugins into .vim/pack/plugins/start
+# - start is for plugins that are to load when vim starts
+# - opt is for plugins that can be loaded when needed
+PLUGIN_BASE="$HOME/.vim/pack/plugins/"
+
+mkdir -p "$PLUGIN_BASE"/{start,opt}
+cd "$PLUGIN_BASE/start"
+
+git clone https://github.com/ctrlpvim/ctrlp.vim
+git clone https://github.com/fatih/vim-go.git
+git clone https://tpope.io/vim/fugitive.git
